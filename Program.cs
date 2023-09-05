617ad290dca1c57d017c54c8dbe3d4cd8a2daa04
jmartinezcode <jmartinez.data@gmail.com>
2023-09-05 14:11:17 -0500

    input number of weeks of data

--- Program.cs
+++ Program.cs
@@ -3,14 +3,19 @@ Console.WriteLine("Enter 1 to create data file.");
 Console.WriteLine("Enter 2 to parse data.");
 Console.WriteLine("Enter anything else to quit.");
 // input response
-string? resp = Console.ReadLine();
+string resp = Console.ReadLine();
 
 if (resp == "1")
 {
-    // TODO: create data file
+    // create data file
+
+    // ask a question
+    Console.WriteLine("How many weeks of data is needed?");
+    // input the response (convert to int)
+    int weeks = int.Parse(Console.ReadLine());
 }
 else if (resp == "2")
 {
     // TODO: parse data file
-
+ 
 }
